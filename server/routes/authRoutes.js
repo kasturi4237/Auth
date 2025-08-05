c51560e9006e6ad528545703342e28996d477fe7
kasturi4237 <kastum7890@gmail.com>
2025-08-05 22:48:43 +0530

    authroutes for send reset otp and reset password

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { register ,login ,logout } from '../controllers/authController.js';
+import { register ,login ,logout, sendVerifyOtp, verifyEmail, sendResetOtp, resetPassword } from '../controllers/authController.js';
+import userAuth from '../middleware/userAuth.js'
 
 //create router named authrouter
 const authRouter = express.Router();//in the router w'll add diff endpoints
@@ -8,5 +9,11 @@ const authRouter = express.Router();//in the router w'll add diff endpoints
 authRouter.post('/register',register);
 authRouter.post('/login',login);
 authRouter.post('/logout',logout);
+authRouter.post('/send-verify-otp',userAuth , sendVerifyOtp);
+authRouter.post('/verify-account',userAuth , verifyEmail);
+authRouter.post('/is-auth',userAuth , isAuthenticated);
+authRouter.post('/send-reset-otp',sendResetOtp);
+authRouter.post('/reset-password',resetPassword);
+
 
 export default authRouter;
