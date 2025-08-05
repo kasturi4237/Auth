bcbd949cb0cee67ebd1e4f5e021c1f4e7e3c6415
kasturi4237 <kastum7890@gmail.com>
2025-08-05 22:45:55 +0530

    middleware for userAuth

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -7,11 +7,26 @@ const userAuth = async (req,res,next) =>{
         return res.json({success: false, message: 'Not Authorized Login Again'})
 
     }
-
+     //if token is available
     try {
+        //first we will decode the token
+        const tokenDecode = jwt.verify(token, procees.env.JWT_SECRET);
+        //from this decodetoken we have to find id bz for creating the token we have to use the id
+        if(tokenDecode.id){
+            //it will add this id in req body with the property userid
+            req.body.userId = tokenDecode.id
+
+        }else{
+            return res.json({success: false, message: 'Not Authorized Login Again'}) ;//if no tokenid
+        }
+
+        next()
         
     } catch (error) {
-        
+       res.json({success:false,message: error.message}); 
     }
 
 }
+
+export default userAuth;
+//using this middleware and controller function we will create the api endpoints lets open authroutes.js
